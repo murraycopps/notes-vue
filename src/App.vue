c6e08814df0d8a161b831e00e703dad44390cdb6
murraycopps <murraycopps@gmail.com>
2022-12-31 23:22:18 -0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NoteCard name="My Notecard" text="This is my notecard text." />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NoteCard from './components/NoteCard.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    NoteCard,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Your styles go here */
 </style>
---
+++ src/components/NoteCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card bg-yellow-300 shadow-lg rounded-md p-8 text-center">
+    <h1 class="text-2xl font-bold text-gray-800 mb-4">{{ name }}</h1>
+    <p class="text-lg text-gray-700">{{ text }}</p>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+  
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+.card{
+  width: clamp(300px, 50%, 500px);
+  min-height: 300px;
+}
+</style>
+  
